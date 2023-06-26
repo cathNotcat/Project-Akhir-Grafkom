6d83b8f5fee7ae3c059eef42621475cfa09f1e9e
benita <109541073+benit11@users.noreply.github.com>
2023-06-26 08:26:30 +0700

    Add files via upload

--- LanjutanMixue/src/main/java/Engine/Quadric.java
+++ LanjutanMixue/src/main/java/Engine/Quadric.java
@@ -1272,64 +1272,6 @@ public class Quadric extends Circle{
 
         uniformsMap.setUniform("viewPos", camera.getPosition());
     }
-//   public void drawSetup(Camera camera, Projection projection){
-//       super.drawSetup(camera, projection);
-
-//       // Bind NBO
-//       glEnableVertexAttribArray(1);
-//       glBindBuffer(GL_ARRAY_BUFFER, nbo);
-//       glVertexAttribPointer(1,
-//               3, GL_FLOAT,
-//               false,
-//               0, 0);
-///        uniformsMap.setUniform(
-///                "lightColor",
-///                new Vector3f(0.8f,0.8f,0.8f));
-///        uniformsMap.setUniform(
-///                "lightPos",
-///                new Vector3f(0f,0.0f,0.0f));
-//       uniformsMap.setUniform("dirLight.direction", new Vector3f(-0.2f, -1.0f, -0.3f));
-//       uniformsMap.setUniform("dirLight.ambient", new Vector3f(0.05f, 0.05f, 0.05f));
-//       uniformsMap.setUniform("dirLight.diffuse", new Vector3f(0.4f, 0.4f, 0.4f));
-//       uniformsMap.setUniform("dirLight.specular", new Vector3f(0.5f, 0.5f, 0.5f));
-
-//       //posisi pointLight
-//       Vector3f[] _pointLightPosition = {
-//               new Vector3f(0f, 2f, 0f),
-//               new Vector3f(0f, 0f, 0f),
-//               new Vector3f(0f, 2f, 0f),
-//               new Vector3f(0f, 2f, 0f)
-//       };
-//       for(int i = 0; i < _pointLightPosition.length; i++){
-//           uniformsMap.setUniform("pointLights["+i+"].position", _pointLightPosition[i]);
-//           uniformsMap.setUniform("pointLights["+i+"].ambient", new Vector3f(0.1f, 0.1f, 0.1f));
-//           uniformsMap.setUniform("pointLights["+i+"].diffuse", new Vector3f(0.3f, 0.3f, 0.3f));
-//           uniformsMap.setUniform("pointLights["+i+"].specular", new Vector3f(0.6f, 0.6f, 0.6f));
-//           uniformsMap.setUniform("pointLights["+i+"].constant", 1.0f);
-//           uniformsMap.setUniform("pointLights["+i+"].linear", 0.09f);
-//           uniformsMap.setUniform("pointLights["+i+"].quadratic", 0.032f);
-
-
-
-//       };
-
-//       //warnaa ... harus lebih terang dari ambient
-
-//       //spotLight
-//       uniformsMap.setUniform("spotLight.position", camera.getPosition());
-//       uniformsMap.setUniform("spotLight.direction", camera.getDirection());
-//       uniformsMap.setUniform("spotLight.ambient", new Vector3f(0.1f, 0.1f, 0.1f));
-//       uniformsMap.setUniform("spotLight.diffuse", new Vector3f(1.0f, 1.0f, 1.0f));
-//       uniformsMap.setUniform("spotLight.specular", new Vector3f(1.0f, 1.0f, 1.0f));
-//       uniformsMap.setUniform("spotLight.constant", 1.0f);
-//       uniformsMap.setUniform("spotLight.linear", 0.09f);
-//       uniformsMap.setUniform("spotLight.quadratic", 0.09f);
-//       uniformsMap.setUniform("spotLight.cutOff", (float) Math.cos(Math.toRadians(12.5f)));
-//       uniformsMap.setUniform("spotLight.outerCutOff", (float) Math.cos(Math.toRadians(15f)));
-
-
-//       uniformsMap.setUniform("viewPos", camera.getPosition());
-//   }
 
 //    public void draw(){
 //        drawSetup();
